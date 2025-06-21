c04e14ed99a5dd43c85f86c6c4d677a23e4fd6ac
adamcaak <ladamczak1997@gmail.com>
2025-06-21 23:17:19 +0200

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Łukasz Adamczak on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
