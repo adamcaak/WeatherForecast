08ba05635347009f0f21014c7ce4327ef290d374
adamcaak <ladamczak1997@gmail.com>
2025-06-21 23:47:23 +0200

    Update ContentView

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 50, low: 30)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 40, low: 20)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+            
+    var body: some View {
+        VStack {
+            Text(day)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+            Text("High: \(high)°C")
+            Text("Low: \(low)°C")
+        }
+        .padding()
+    }
 }
